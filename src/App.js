7b52e87546984ef13b6f0afe9ad3276a9ec14e91
Pablo silva <fiatkowskiunico@hotmail.com>
2023-01-27 22:15:13 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
       <div className="App">
-      <Header> <h1>teste</h1></Header> 
-       <Home></Home>
-       <Footer></Footer>
+      <Header> <h1>tessste</h1></Header> 
+       <Home><h1>eae</h1></Home>
+       <Footer><h1>skkdsadsksksk</h1></Footer>
     </div>
   
   );

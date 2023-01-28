65a3cd5fb768b6401d700357a44f2110a90997ce
Pablo silva <fiatkowskiunico@hotmail.com>
2023-01-27 22:16:31 -0400

    Main

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>

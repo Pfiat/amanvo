547dac4595198afd821761fba2eafd6ddfd802f8
Pablo silva <fiatkowskiunico@hotmail.com>
2023-01-27 22:06:40 -0400

    Main primeiro passo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-        <Header />
-        <Home />
-        <Footer />
+      <Header></Header> 
+       <Home></Home>
+       <Footer></Footer>
     </div>
   
   );

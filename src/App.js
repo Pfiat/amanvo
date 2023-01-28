3942e85a0ec40be3fb0b75df72d0dacb256e4b5d
Pablo silva <fiatkowskiunico@hotmail.com>
2023-01-27 22:28:36 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Footer from './components/Footer';
-
 function App() {
   return (
-      <div className="App">
-      <Header> <h1>tessste</h1></Header> 
-       <Home><h1>eae</h1></Home>
-       <Footer><h1>skkdsadsksksk</h1></Footer>
+    <div className="App">
+      <h1>TESTE PROJETO</h1>
     </div>
   
   );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
-import Header from './components/Header';
-import Home from './components/Home';
-import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals(sendToVercelAnalytics);

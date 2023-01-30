60ff070b5260a1f07a10aab0d8fabb3de3decf22
Pablo silva <fiatkowskiunico@hotmail.com>
2023-01-29 22:00:08 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import { inject } from '@vercel/analytics';
 
-inject();
 
 function HomePage() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.render(
   <React.StrictMode>
     <App />

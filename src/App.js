9701183d675ea111334cb64533485fb6b3eeb72b
Pablo silva <fiatkowskiunico@hotmail.com>
2023-01-29 21:21:56 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h1>TESTE PROJETO</h1>
+    
+
     </div>
   
   );
